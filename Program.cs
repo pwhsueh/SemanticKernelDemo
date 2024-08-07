73f70c07efcacc916e3a5cd0a5f12aff7267bc0e
PWHsueh <pwhsueh@gmail.com>
2024-08-07 20:42:53 +0800

    Exercise - Use built-in plugins

--- Program.cs
+++ Program.cs
@@ -12,7 +12,20 @@ builder.AddOpenAIChatCompletion(
 );
 
 #pragma warning disable SKEXP0050 
-builder.Plugins.AddFromType<TimePlugin>();
+
+builder.Plugins.AddFromType<ConversationSummaryPlugin>();
+
 var kernel = builder.Build();
-var currentDay = await kernel.InvokeAsync("TimePlugin", "DayOfWeek");
-Console.WriteLine(currentDay);
+
+string input = @" Alice: Hi Bob, how are you doing today?
+                Bob: I'm doing great, Alice! How about you?
+                Alice: I'm good too, thanks for asking. Have you finished the report yet?
+                Bob: Yes, I finished it last night. I'll send it to you shortly.
+                Alice: Awesome, thanks!";
+
+var result = await kernel.InvokeAsync(
+    "ConversationSummaryPlugin",
+    "SummarizeConversation",
+    new() { { "input", input } });
+
+Console.WriteLine(result);
