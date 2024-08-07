e3ca294095597ebe26e71e63db6104ff795aab65
PWHsueh <pwhsueh@gmail.com>
2024-08-07 17:49:52 +0800

    Exercise - Build a kernel object

--- Program.cs
+++ Program.cs
@@ -10,3 +10,6 @@ builder.AddOpenAIChatCompletion(
     modelId: "gpt-4o" // optional
 );
 var kernel = builder.Build();
+var result = await kernel.InvokePromptAsync(
+        "請給我一份包含雞蛋和起司的早餐食物清單。");
+Console.WriteLine(result);
