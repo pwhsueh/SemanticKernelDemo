fe50f65994986beb59618c57ecfe60a02158b8df
PWHsueh <pwhsueh@gmail.com>
2024-08-07 17:45:02 +0800

    How to build your kernel

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SemanticKernel;
+
+const string api_Key = "";
+
+
+// Create kernel
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(
+    apiKey: api_Key,
+    modelId: "gpt-4o" // optional
+);
+var kernel = builder.Build();
