9b7d71253a79265b49dc6a28f79316c3884e0941
PWHsueh <pwhsueh@gmail.com>
2024-08-07 20:59:40 +0800

    Save prompts to files

--- Program.cs
+++ Program.cs
@@ -15,20 +15,11 @@ builder.AddOpenAIChatCompletion(
 
 
 var kernel = builder.Build();
+var plugins = kernel.CreatePluginFromPromptDirectory("Prompts");
+string input = "G, C";
 
+var result = await kernel.InvokeAsync(
+    plugins["SuggestChords"],
+    new() { { "startingChords", input } });
 
-string input = @"我有從6月1日到7月22日的假期。我想去希臘。我住在台灣。";
-
-string prompt = @$"
-<message role=""system"">指示：從使用者的請求中識別出發地和目的地以及日期
-</message>
-
-<message role=""user"">你能給我一份從西雅圖到東京的航班列表嗎？
-我想從3月11日到3月18日旅行。</message>
-
-<message role=""assistant"">西雅圖|東京|2024/03/11|2024/03/18
-</message>
-
-<message role=""user"">${input}</message>";
-var result = await kernel.InvokePromptAsync(prompt);
 Console.WriteLine(result);
