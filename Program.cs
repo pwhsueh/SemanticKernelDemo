7e19ce2f9b088070b9bc3db98b452a840da5c61a
PWHsueh <pwhsueh@gmail.com>
2024-08-07 20:49:17 +0800

    Exercise - Write your own prompt

--- Program.cs
+++ Program.cs
@@ -13,19 +13,19 @@ builder.AddOpenAIChatCompletion(
 
 #pragma warning disable SKEXP0050 
 
-builder.Plugins.AddFromType<ConversationSummaryPlugin>();
 
 var kernel = builder.Build();
 
-string input = @" Alice: Hi Bob, how are you doing today?
-                Bob: I'm doing great, Alice! How about you?
-                Alice: I'm good too, thanks for asking. Have you finished the report yet?
-                Bob: Yes, I finished it last night. I'll send it to you shortly.
-                Alice: Awesome, thanks!";
 
-var result = await kernel.InvokeAsync(
-    "ConversationSummaryPlugin",
-    "SummarizeConversation",
-    new() { { "input", input } });
+string language = "英文";
+string history = @"我和孩子一起旅行，其中一個孩子對花生過敏。";
 
+string prompt = @$"考慮旅行者的背景：
+    ${history}
+
+    創建一個列表，列出旅行者在${language}中可能會發現有用的短語和詞彙。
+
+    按類別分組短語。包括常見的方向詞。以以下格式顯示短語：
+    你好 - Bonjour [bohn-zhoor]";
+var result = await kernel.InvokePromptAsync(prompt);
 Console.WriteLine(result);
