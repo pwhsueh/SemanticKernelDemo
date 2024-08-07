52984f020f42b581ebac08d2992f59f3ab08ebd0
PWHsueh <pwhsueh@gmail.com>
2024-08-07 22:52:10 +0800

    Exercise - Saving prompts to files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Plugins.Core;
 
 const string api_Key = "";
@@ -15,11 +16,25 @@ builder.AddOpenAIChatCompletion(
 
 
 var kernel = builder.Build();
-var plugins = kernel.CreatePluginFromPromptDirectory("Prompts");
-string input = "G, C";
+var prompts = kernel.ImportPluginFromPromptDirectory("Prompts/TravelPlugins");
 
-var result = await kernel.InvokeAsync(
-    plugins["SuggestChords"],
-    new() { { "startingChords", input } });
+ChatHistory history = [];
+string input = @"我正在計劃和女朋友一起的紀念交往周年旅行。我們喜歡徒步旅行、山脈和海灘。我們的旅行預算是台幣$50000。";
 
+var result = await kernel.InvokeAsync<string>(prompts["SuggestDestinations"],
+    new() { { "input", input } });
+
+Console.WriteLine(result);
+history.AddUserMessage(input);
+history.AddAssistantMessage(result);
+
+Console.WriteLine("您想去哪裡？");
+input = Console.ReadLine();
+
+result = await kernel.InvokeAsync<string>(prompts["SuggestActivities"],
+    new() {
+        { "history", history },
+        { "destination", input },
+    }
+);
 Console.WriteLine(result);
