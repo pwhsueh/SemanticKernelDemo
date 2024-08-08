77c5470471ce4aa33c7109bf4ee900c4740e9fdd
PWHsueh <pwhsueh@gmail.com>
2024-08-08 09:14:27 +0800

    Exercise - Automatically invoke functions

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Planning.Handlebars;
 using Microsoft.SemanticKernel.Plugins.Core;
 using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
@@ -21,50 +22,14 @@ kernel.ImportPluginFromType<MusicLibraryPlugin>();
 kernel.ImportPluginFromType<MusicConcertPlugin>();
 kernel.ImportPluginFromPromptDirectory("Prompts");
 
-var planner = new HandlebarsPlanner(new HandlebarsPlannerOptions() { AllowLoops = true });
+OpenAIPromptExecutionSettings settings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
 
-string location = "台北";
-string goal = @$"根據用戶最近播放的音樂，推薦一場位於${location}的音樂會";
+string prompt = @"我住在台北。根據我最近播放的歌曲和即將舉行的音樂會清單，您推薦哪場音樂會？
+";
 
-var concertPlan = await planner.CreatePlanAsync(kernel, goal);
-// var result = await plan.InvokeAsync(kernel);
+var result = await kernel.InvokePromptAsync(prompt, new(settings));
 
-// output the plan result
-// Console.WriteLine("Concert Plan:");
-// Console.WriteLine(concertPlan);
-
-var songSuggesterFunction = kernel.CreateFunctionFromPrompt(
-    promptTemplate: @"根據用戶最近播放的音樂：
-        {{$recentlyPlayedSongs}}
-        從音樂庫中推薦一首歌曲給用戶：
-        {{$musicLibrary}}
-        ",
-    functionName: "SuggestSong",
-    description: "向用戶推薦一首歌曲"
-);
-
-kernel.Plugins.AddFromFunctions("SuggestSongPlugin", [songSuggesterFunction]);
-
-var songSuggestPlan = await planner.CreatePlanAsync(kernel, @"根據用戶最近播放的歌曲，從音樂庫中向用戶推薦一首歌曲
-");
-
-// Console.WriteLine("Song Plan:");
-// Console.WriteLine(songSuggestPlan);
-string dir = Directory.GetCurrentDirectory();
-string template = File.ReadAllText($"{dir}/handlebarsTemplate.txt");
-
-var handlebarsPromptFunction = kernel.CreateFunctionFromPrompt(
-    new()
-    {
-        Template = template,
-        TemplateFormat = "handlebars"
-    }, new HandlebarsPromptTemplateFactory()
-);
-
-var templateResult = await kernel.InvokeAsync(handlebarsPromptFunction,
-    new() {
-        { "location", location },
-        { "suggestConcert", true }
-    });
-
-Console.WriteLine(templateResult);
+Console.WriteLine(result);
